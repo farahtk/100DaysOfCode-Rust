153fadd0bac895f104bf2381fae38394ae7d448e
Farah Kuhurrum <farahkhurrum@gmail.com>
2020-09-07 14:04:47 -0700

    adding the debug information

--- target/rls/debug/deps/test1-0c69e67112563b21.d
+++ target/rls/debug/deps/test1-0c69e67112563b21.d
@@ -1,5 +1,5 @@
-/home/farah/farah/rustTest/test1/target/rls/debug/deps/test1-0c69e67112563b21.rmeta: src/main.rs
+/home/farah/farah/100DaysOfCode-Rust/target/rls/debug/deps/test1-0c69e67112563b21.rmeta: src/main.rs
 
-/home/farah/farah/rustTest/test1/target/rls/debug/deps/test1-0c69e67112563b21.d: src/main.rs
+/home/farah/farah/100DaysOfCode-Rust/target/rls/debug/deps/test1-0c69e67112563b21.d: src/main.rs
 
 src/main.rs:
--- target/rls/debug/deps/test1-4e94de14c1c94861.d
+++ target/rls/debug/deps/test1-4e94de14c1c94861.d
@@ -1,5 +1,5 @@
-/home/farah/farah/rustTest/test1/target/rls/debug/deps/test1-4e94de14c1c94861.rmeta: src/main.rs
+/home/farah/farah/100DaysOfCode-Rust/target/rls/debug/deps/test1-4e94de14c1c94861.rmeta: src/main.rs
 
-/home/farah/farah/rustTest/test1/target/rls/debug/deps/test1-4e94de14c1c94861.d: src/main.rs
+/home/farah/farah/100DaysOfCode-Rust/target/rls/debug/deps/test1-4e94de14c1c94861.d: src/main.rs
 
 src/main.rs:
